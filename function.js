eca38cc9fd72f48e7f28c278052afbdace13ab6a
B srikanth reddy <bayagarisrikanthreddy809@gmail.com>
2023-04-16 10:31:37 +0530

    add functions

---
+++ function.js
@@ -0,0 +1,10 @@
+
+function bike(a, b, c) {
+                    console.log("Model" + " " + a + " " + "Company" + " " + b + " " + "Price" + " " + c);
+                    return a;
+}
+bike(2020, "srikanth", 90000);
+bike(2021, "KTM", 200000);
+console.log(21);
+
+
