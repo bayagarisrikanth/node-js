9d1f76a047a23cdf7b6c0cbde1234cc9b1952599
B srikanth reddy <bayagarisrikanthreddy809@gmail.com>
2023-04-16 10:49:46 +0530

    add two programs

---
+++ pattern1.js
@@ -0,0 +1,13 @@
+/*var row = i;
+var col = j;*/
+
+for (var i = 1; i <= 5; i++) {
+                    var row="";
+                    for (var j = 1; j <= 5; j++) {
+                                        row+='*';
+                                        
+
+
+                    }
+                    console.log(row);
+}
